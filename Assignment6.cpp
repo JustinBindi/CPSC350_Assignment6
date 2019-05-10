142d8037d07faca6f7b7fe94276e0bd891d4787e
Justin Bindi <bindi@chapman.edu>
2019-05-10 03:03:11 +0000

    basically done

---
+++ Assignment6.cpp
@@ -0,0 +1,259 @@
+#include "Assignment6.h"
+#include <iostream>
+#include <fstream>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+BubbleSort::BubbleSort()
+{
+
+}
+
+BubbleSort::BubbleSort(double g)
+{
+
+}
+
+/*void BubbleSort::switchNumbersB(int num1, int num2)
+{
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+}*/
+
+void BubbleSort::runBubbleSort(double array[], int n)
+{
+  for (int i = 0; i < n-1; i++)
+  {
+    // Last i elements are already in place
+    for (int j = 0; j < n-i-1; j++)
+    {
+      if (array[j] > array[j+1])
+      {
+        swap(array[j], array[j+1]);
+      }
+    }
+  }
+}
+
+SelectionSort::SelectionSort()
+{
+
+}
+
+SelectionSort::SelectionSort(double g)
+{
+
+}
+
+/*void SelectionSort::switchNumbersS(int num1, int num2)
+{
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+}*/
+
+void SelectionSort::runSelectionSort(double array[], int n)
+{
+  // One by one move boundary of unsorted subarray
+  for (int i = 0; i < n-1; i++)
+  {
+    // Find the minimum element in unsorted array
+    int minIndex = i;
+    for (int j = i+1; j < n; j++)
+    {
+      if (array[j] < array[minIndex])
+      {
+        minIndex = j;
+      }
+    }
+
+    // Swap the found minimum element with the first element
+    swap(array[minIndex], array[i]);
+  }
+
+}
+
+InsertionSort::InsertionSort()
+{
+
+}
+
+InsertionSort::InsertionSort(double g)
+{
+
+}
+
+void InsertionSort::runInsertionSort(double array[], int n)
+{
+  for (int i = 1; i < n; i++)
+  {
+      int key = array[i];
+      int j = i - 1;
+
+      /* Move elements of array[0..i-1], that are
+      greater than key, to one position ahead
+      of their current position */
+      while (j >= 0 && array[j] > key)
+      {
+          array[j + 1] = array[j];
+          j = j - 1;
+      }
+      array[j + 1] = key;
+    }
+}
+
+QuickSort::QuickSort()
+{
+
+}
+
+QuickSort::QuickSort(double g)
+{
+
+}
+
+/* This function takes last element as pivot, places
+   the pivot element at its correct position in sorted
+    array, and places all smaller (smaller than pivot)
+   to left of pivot and all greater elements to right
+   of pivot */
+int QuickSort::partition (double array[], int low, int high)
+{
+    int pivot = array[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+
+    for (int j = low; j <= high- 1; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (array[j] <= pivot)
+        {
+            i++;    // increment index of smaller element
+            swap(array[i], array[j]);
+        }
+    }
+    swap(array[i + 1], array[high]);
+    return (i + 1);
+}
+
+void QuickSort::runQuickSort(double array[], int low, int high)
+{
+  if (low < high)
+    {
+        /* pi is partitioning index, array[p] is now
+           at right place */
+        int partitionIndex = partition(array, low, high);
+
+        // Separately sort elements before
+        // partition and after partition
+        runQuickSort(array, low, partitionIndex - 1);
+        runQuickSort(array, partitionIndex + 1, high);
+    }
+}
+
+Simulator::Simulator()
+{
+
+}
+
+Simulator::Simulator(int b)
+{
+
+}
+
+void Simulator::Run()
+{
+  cout << "What is the name of the file?" << endl;
+  cin >> answer;
+
+  ifstream myFile (answer);
+  if (myFile)
+  {
+    string firstLine;
+    getline (myFile, firstLine);
+    length = stoi(firstLine);
+  }
+
+  double array[length];
+  double array2[length];
+  double array3[length];
+  double array4[length];
+
+  if (myFile)
+  {
+    for(int i = 0; i < length; ++i)
+    {
+      myFile >> array[i];
+    }
+  }
+
+  for(int i = 0; i < length; ++i)
+  {
+    array2[i] = array[i];
+    array3[i] = array[i];
+    array4[i] = array[i];
+  }
+
+
+
+  BubbleSort bubble;
+  SelectionSort select;
+  InsertionSort insert;
+  QuickSort quick;
+
+  int n = sizeof(array)/sizeof(array[0]);
+
+  //BubbleSort
+  auto start = high_resolution_clock::now();
+  bubble.runBubbleSort(array, n);
+  auto stop = high_resolution_clock::now();
+
+  auto duration = duration_cast<microseconds>(stop - start);
+
+  cout << "" << endl;
+  cout << "BubbleSort: " << endl;
+  cout << "Time taken by function: "
+           << duration.count() << " microseconds" << endl;
+  cout << "" << endl;
+
+  //SelectionSort
+  start = high_resolution_clock::now();
+  select.runSelectionSort(array2, n);
+  stop = high_resolution_clock::now();
+
+  duration = duration_cast<microseconds>(stop - start);
+
+  cout << "SelectionSort: " << endl;
+  cout << "Time taken by function: "
+          << duration.count() << " microseconds" << endl;
+  cout << "" << endl;
+
+  //InsertionSort
+  start = high_resolution_clock::now();
+  insert.runInsertionSort(array3, n);
+  stop = high_resolution_clock::now();
+
+  duration = duration_cast<microseconds>(stop - start);
+
+  cout << "InsertionSort: " << endl;
+  cout << "Time taken by function: "
+           << duration.count() << " microseconds" << endl;
+  cout << "" << endl;
+
+  //QuickSort
+  start = high_resolution_clock::now();
+  quick.runQuickSort(array4, 0, n-1);
+  stop = high_resolution_clock::now();
+
+  duration = duration_cast<microseconds>(stop - start);
+
+  cout << "QuickSort: " << endl;
+  cout << "Time taken by function: "
+           << duration.count() << " microseconds" << endl;
+  cout << "" << endl;
+
+
+}
---
+++ Assignment6.h
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+class BubbleSort
+{
+  public:
+    BubbleSort();
+    BubbleSort(double g);
+    void runBubbleSort(double array[], int n);
+    //void switchNumbersB(int num1, int num2);
+};
+
+class SelectionSort
+{
+  public:
+    SelectionSort();
+    SelectionSort(double g);
+    void runSelectionSort(double array[], int n);
+    void switchNumbersS(int num1, int num2);
+};
+
+class InsertionSort
+{
+  public:
+    InsertionSort();
+    InsertionSort(double g);
+    void runInsertionSort(double array[], int n);
+};
+
+class QuickSort
+{
+  public:
+    QuickSort();
+    QuickSort(double g);
+    void runQuickSort(double array[], int low, int high);
+    int partition (double array[], int low, int high);
+};
+
+class Simulator
+{
+  public:
+    Simulator();
+    Simulator(int b);
+    void Run();
+    string answer;
+    int length;
+
+};
